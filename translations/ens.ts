feb2785b13acacfe95f1d749cf631a73e3a06ae0
anurag <anurag.dhingra@wealth-park.com>
2023-10-23 04:31:12 +0900

    fix: message bug with image gallary

---
+++ translations/ens.ts
@@ -0,0 +1,6 @@
+const translationsMany: { [key: string]: string[] } = {
+    exampleMessages: [
+    'What do you recommend from the menu?',
+    'Do you have any vegetarian items on the menu?',
+    ]
+  };
